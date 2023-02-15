9efc7b7356f52619c62d86b5305b9cac7db50ef1
David T. Pizarro Frick <davidpizarrofrick@gmail.com>
2023-02-15 23:37:35 +0100

    feat(footerComponent)

---
+++ src/views/components/main/desktop/layout/footerMain/FooterMain.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './FooterMain.scss';
+
+function FooterMain(props) {
+  return (
+    <div className='footer-main'>
+      
+    </div>
+  );
+}
+
+export default FooterMain;
