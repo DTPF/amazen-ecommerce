d3a73c800c11af7db0a2fbe942b7890eb8158dff
David T. Pizarro Frick <davidpizarrofrick@gmail.com>
2023-02-16 23:30:57 +0100

    feat(stores): add stores page

---
+++ src/views/pages/main/stores/Stores.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import HelmetSEO from "../../../utils/helmetSEO/HelmetSEO";
+import './Stores.scss';
+
+export default function Stores() {
+  return (
+    <HelmetSEO title={'Amazén | Amazén Basics'}>
+      <div className='stores'>
+        <h1>Amazén Basics</h1>
+      </div>
+    </HelmetSEO>
+  );
+}
