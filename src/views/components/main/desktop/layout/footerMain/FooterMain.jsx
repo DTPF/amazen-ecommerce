bf04ce8fba8cdbf80104412cc487851bae1fc1a8
David T. Pizarro Frick <davidpizarrofrick@gmail.com>
2023-02-16 23:37:11 +0100

    update: footer content and style

--- src/views/components/main/desktop/layout/footerMain/FooterMain.jsx
+++ src/views/components/main/desktop/layout/footerMain/FooterMain.jsx
@@ -4,7 +4,7 @@ import './FooterMain.scss';
 function FooterMain(props) {
   return (
     <div className='footer-main'>
-      
+      <div className='footer-main__copyright'>© 1996-2023, Amazén.com, Inc. o sus afiliados</div>
     </div>
   );
 }
