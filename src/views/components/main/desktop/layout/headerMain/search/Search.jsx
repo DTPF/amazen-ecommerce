bc055e1e466bcb49293ef9a683e8ca37f9e64b33
David T. Pizarro Frick <davidpizarrofrick@gmail.com>
2023-02-16 17:53:21 +0100

    feat(search): add component

---
+++ src/views/components/main/desktop/layout/headerMain/search/Search.jsx
@@ -0,0 +1,53 @@
+import React from 'react';
+import { BsSearch } from 'react-icons/bs';
+import './Search.scss';
+
+export default function Search(props) {
+  return (
+    <div className='search'>
+      <div className="search__input-group">
+        <span className="search__input-group--select">
+          <select>
+            <option>Todos los departamentos</option>
+            <option>Alimentación y bebidas</option>
+            <option>Bricolaje y herramientas</option>
+            <option>Electrónica</option>
+            <option>Todos los departamentos</option>
+            <option>Alimentación y bebidas</option>
+            <option>Bricolaje y herramientas</option>
+            <option>Electrónica</option>
+            <option>Todos los departamentos y bebidas alcoholicas</option>
+            <option>Alimentación y bebidas</option>
+            <option>Bricolaje y herramientas</option>
+            <option>Electrónica</option>
+            <option>Todos los departamentos</option>
+            <option>Alimentación y bebidas</option>
+            <option>Bricolaje y herramientas</option>
+            <option>Electrónica</option>
+            <option>Todos los departamentos</option>
+            <option>Alimentación y bebidas</option>
+            <option>Bricolaje y herramientas</option>
+            <option>Electrónica</option>
+            <option>Todos los departamentos</option>
+            <option>Alimentación y bebidas</option>
+            <option>Bricolaje y herramientas</option>
+            <option>Electrónica</option>
+            <option>Todos los departamentos</option>
+            <option>Alimentación y bebidas</option>
+            <option>Bricolaje y herramientas</option>
+            <option>Electrónica</option>
+            <option>Todos los departamentos</option>
+            <option>Alimentación y bebidas</option>
+            <option>Bricolaje y herramientas</option>
+            <option>Electrónica</option>
+          </select>
+        </span>
+
+        <input id="search" type="text" className="search__input-group--input" name="search" placeholder="Buscar Amazon.es" />
+
+        <button className="search__input-group--button"><BsSearch /></button>
+      </div>
+    </div>
+
+  );
+}
