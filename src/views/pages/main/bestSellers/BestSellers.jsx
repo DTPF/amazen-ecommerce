5a00beccd1807d36a2e1c59a271d0e6aa28afeff
David T. Pizarro Frick <davidpizarrofrick@gmail.com>
2023-02-16 18:33:34 +0100

    feat: add SEO with html title

--- src/views/pages/main/bestSellers/BestSellers.jsx
+++ src/views/pages/main/bestSellers/BestSellers.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import HelmetSEO from "../../../utils/helmetSEO/HelmetSEO";
 
 export default function BestSellers(props) {
   return (
-    <div>
-      Best Sellers
-    </div>
+    <HelmetSEO title={'Amazen | Éxito de ventas'}>
+      <div>
+        Best Sellers
+      </div>
+    </HelmetSEO>
   );
 }
--- src/views/pages/main/home/Home.jsx
+++ src/views/pages/main/home/Home.jsx
@@ -1,13 +1,16 @@
 import React, { Suspense, lazy } from "react";
 import './Home.scss';
+import HelmetSEO from "../../../utils/helmetSEO/HelmetSEO";
 const Sections = lazy(() => import('../../../components/main/desktop/home/sections'))
 
 export default function Home(props) {
   return (
-    <div className="home">
+    <HelmetSEO title={'Amazen | Home'}>
       <Suspense fallback={<></>}>
-        <Sections />
+        <div className="home">
+          <Sections />
+        </div>
       </Suspense>
-    </div>
+    </HelmetSEO>
   );
 }
