cbb65299d2eac9991b98abe1732cd2a28ce9b868
David T. Pizarro Frick <davidpizarrofrick@gmail.com>
2023-02-16 17:59:55 +0100

    feat(shoppingCart): add component

---
+++ src/views/components/main/desktop/layout/headerMain/shoppingCart/ShoppingCart.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import shoppingCartImage from '../../../../../../../assets/images/chart.png';
+import './ShoppingCart.scss';
+
+export default function ShoppingCart(props) {
+  return (
+    <div className='shopping-cart'>
+      <span>3</span>
+      <img src={shoppingCartImage} alt={'Shopping Cart'} />
+    </div>
+  );
+}
