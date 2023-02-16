5dd1ada1e180a48175aea50d11ae5b273f074036
David T. Pizarro Frick <davidpizarrofrick@gmail.com>
2023-02-16 17:48:29 +0100

    feat(selectCountry): create component

---
+++ src/views/components/main/desktop/layout/headerMain/selectCountry/SelectCountry.jsx
@@ -0,0 +1,35 @@
+import React, { useState } from 'react';
+import selectCountryImage from '../../../../../../../assets/images/select_country.png';
+import Modal from "../../../../../../components/main/desktop/UI/modal/Modal";
+import { TfiLayoutLineSolid } from 'react-icons/tfi';
+import './SelectCountry.scss';
+
+
+export default function SelectCountry() {
+  const [isVisibleModal, setIsVisibleModal] = useState(false);
+
+  return (
+    <>
+      {isVisibleModal && (
+        <Modal
+          title={'Elige tu ubicación'}
+          isVisible={isVisibleModal}
+          setIsVisible={setIsVisibleModal}
+          className={'select-country__modal'}
+        >
+          <p>Selecciona una dirección de envío para ver las opciones de envío</p>
+          <button>Inicia sesión para ver tus direcciones</button>
+          <div className='select-country__modal--post-code'>
+            <TfiLayoutLineSolid /><TfiLayoutLineSolid /><TfiLayoutLineSolid />
+            <p>o intruduce un código postal de España</p>
+            <TfiLayoutLineSolid /><TfiLayoutLineSolid /><TfiLayoutLineSolid />
+          </div>
+        </Modal>
+      )}
+      <div onClick={() => setIsVisibleModal(!isVisibleModal)} className='select-country'>
+        <img src={selectCountryImage} alt='Select Country' />
+      </div>
+    </>
+  )
+}
+
