1389cacd0f33ab13738f11ce9826cf8db316319b
David T. Pizarro Frick <davidpizarrofrick@gmail.com>
2023-02-16 23:39:28 +0100

    add helmet and style to bestsellers

--- src/views/pages/main/bestSellers/BestSellers.jsx
+++ src/views/pages/main/bestSellers/BestSellers.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import HelmetSEO from "../../../utils/helmetSEO/HelmetSEO";
+import './BestSellers.scss';
 
-export default function BestSellers(props) {
+export default function BestSellers() {
   return (
-    <HelmetSEO title={'Amazen | Éxito de ventas'}>
-      <div>
-        Best Sellers
+    <HelmetSEO title={'Amazén | Éxito de ventas'}>
+      <div className='best-sellers'>
+        <h1>Éxito de ventas</h1>
       </div>
     </HelmetSEO>
   );
