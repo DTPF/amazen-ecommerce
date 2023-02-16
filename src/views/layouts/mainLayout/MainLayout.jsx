61a98c870b82d196b8762666c913a53b91a27895
David T. Pizarro Frick <davidpizarrofrick@gmail.com>
2023-02-16 23:38:53 +0100

    import: menu component

--- src/views/layouts/mainLayout/MainLayout.jsx
+++ src/views/layouts/mainLayout/MainLayout.jsx
@@ -3,12 +3,14 @@ import { DOMSubRoutes } from '../../../config/DOMRoutes';
 import './MainLayout.scss';
 const HeaderMain = lazy(() => import('../../components/main/desktop/layout/headerMain'));
 const FooterMain = lazy(() => import('../../components/main/desktop/layout/footerMain'));
+const MenuMain = lazy(() => import('../../components/main/desktop/layout/menuMain/MenuMain'));
 
 export default function MainLayout({ routes }) {
   return (
     <div className='main-layout'>
       <Suspense fallback={<></>}>
         <HeaderMain />
+        <MenuMain />
 
         <div className='main-layout__main'>
           <DOMSubRoutes routes={routes} /> {/* Switch views/pages/main */}
