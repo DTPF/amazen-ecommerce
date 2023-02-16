a4d164d9cedd39dac3ec2287a51d77e54e42d110
David T. Pizarro Frick <davidpizarrofrick@gmail.com>
2023-02-16 17:43:22 +0100

    feat: add categories component to home page

--- src/views/pages/main/home/Home.jsx
+++ src/views/pages/main/home/Home.jsx
@@ -1,9 +1,13 @@
-import React from 'react';
+import React, { Suspense, lazy } from "react";
+import './Home.scss';
+const Sections = lazy(() => import('../../../components/main/desktop/home/sections'))
 
 export default function Home(props) {
   return (
-    <div>
-      Home
+    <div className="home">
+      <Suspense fallback={<></>}>
+        <Sections />
+      </Suspense>
     </div>
   );
 }
