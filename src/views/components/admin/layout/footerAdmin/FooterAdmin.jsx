bd4dbfd7c27c6ad58018331c9093fdff76936079
David T. Pizarro Frick <davidpizarrofrick@gmail.com>
2023-02-17 23:52:04 +0100

    feat(adminFooter): add component

---
+++ src/views/components/admin/layout/footerAdmin/FooterAdmin.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './FooterAdmin.scss';
+
+export default function FooterAdmin() {
+  return (
+    <div className='footer-admin'>
+      <div className='footer-admin__copyright'>© 1996-2023, Amazén.com, Inc. o sus afiliados</div>
+    </div>
+  );
+}
