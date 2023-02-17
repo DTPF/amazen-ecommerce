7d23c3a60d4e56f437fb8eb28b7ad7e3cc97759d
David T. Pizarro Frick <davidpizarrofrick@gmail.com>
2023-02-17 23:59:53 +0100

    feat: add myAccount and orderHistory components

--- src/views/components/main/desktop/layout/headerMain/HeaderMain.jsx
+++ src/views/components/main/desktop/layout/headerMain/HeaderMain.jsx
@@ -4,6 +4,8 @@ import SelectCountry from './selectCountry/SelectCountry';
 import Search from './search/Search';
 import Chart from './shoppingCart/ShoppingCart';
 import './HeaderMain.scss';
+import OrderHistory from './oderHistory/OrderHistory';
+import MyAccount from './myAccount/MyAccount';
 
 export default function HeaderMain() {
   return (
@@ -11,6 +13,8 @@ export default function HeaderMain() {
       <Logo />
       <SelectCountry />
       <Search />
+      <MyAccount />
+      <OrderHistory />
       <Chart />
     </div>
   );
