5a92438df6dbd34e703f11e615c20b239bf94154
David T. Pizarro Frick <davidpizarrofrick@gmail.com>
2023-02-17 23:44:33 +0100

    change(error): add error view

--- src/views/pages/error/Error.jsx
+++ src/views/pages/error/Error.jsx
@@ -1,9 +1,21 @@
 import React from 'react';
+import { Button, Result } from 'antd';
+import './Error.scss';
 
 export default function Error() {
   return (
-    <div>
-      Error 404
+    <div className='error'>
+      <Result
+        status="404"
+        title="404"
+        subTitle="Lo sentimos, la página que buscas no existe."
+        extra={
+          <Button type="primary" onClick={() => window.history.back()}>
+            Volver
+          </Button>
+        }
+      />
     </div>
+
   );
 }
