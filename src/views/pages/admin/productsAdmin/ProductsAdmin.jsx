915a0e9ce072c39b5cb09cfcdbdddd8af99d79d1
David T. Pizarro Frick <davidpizarrofrick@gmail.com>
2023-02-18 00:05:44 +0100

    feat(adminProducts): add page component

---
+++ src/views/pages/admin/productsAdmin/ProductsAdmin.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './ProductsAdmin.scss';
+
+export default function ProductsAdmin(props) {
+  return (
+    <div className='products-admin'>
+      <h1 className='products-admin__title'>Products</h1>
+    </div>
+  );
+}
