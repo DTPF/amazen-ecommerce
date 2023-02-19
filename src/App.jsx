a70d3fe2ff7044e20d86b2b5536895ac085cbcac
David T. Pizarro Frick <davidpizarrofrick@gmail.com>
2023-02-19 17:06:18 +0100

    invoke indexedDB

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,9 @@
 import Routes from './config/Routes';
+import indexedDB from './indexedDB';
 
 export default function App() {
+  // Create indexedDb and insert first data
+  indexedDB();
   // Switch views/layouts
   return (
     <Routes />
