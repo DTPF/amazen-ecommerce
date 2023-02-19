1f12079db2de4b0f0667b1c465dbd36343b212b3
David T. Pizarro Frick <davidpizarrofrick@gmail.com>
2023-02-19 17:11:39 +0100

    feat(firstDBInsert): add sections

---
+++ src/indexedDB/api/firstInsertDB/sections/firstPostSections.js
@@ -0,0 +1,41 @@
+import { SECTIONS } from "../../../config";
+import { sectionsData } from "./sectionsData";
+
+export default function firstPostSections(vls, db) {
+  if (vls === 1 || vls < db.version) {
+    sectionsData.forEach((section, index) => {
+      const { title, img, linkName, link, isActive } = section;
+      postSections(db, title, img, linkName, link, isActive, index);
+      localStorage.setItem("amazen_idb_vs", db.version);
+    });
+  }
+}
+
+function postSections(db, title, img, linkName, link, isActive, index) {
+  let transaction = db.transaction([SECTIONS], "readwrite");
+  let sections = transaction.objectStore(SECTIONS);
+  let section = {
+    id: index,
+    title: title,
+    img: img,
+    linkName: linkName,
+    link: link,
+    isActive: isActive
+  };
+  let request = sections.put(section);
+
+  request.onsuccess = function (e) {
+    const lsObj = localStorage.getItem('last_id');
+    const objParse = JSON.parse(lsObj);
+    const obj = {
+      users: objParse && objParse.users,
+      sections: e.target.result
+    }
+
+    localStorage.setItem('last_id', JSON.stringify(obj));
+  };
+
+  request.onerror = function () {
+    console.log("Error", request.error);
+  };
+}
---
+++ src/indexedDB/api/firstInsertDB/sections/sectionsData.js
@@ -0,0 +1,67 @@
+import echoShowImg from '../../../../uploads/sections/echo-show.png';
+import gatoProgramadorImg from '../../../../uploads/sections/gato-programador.jpg';
+import helpImg from '../../../../uploads/sections/ayuda.png';
+import informaticaImg from '../../../../uploads/sections/informatica.png';
+import outletImg from '../../../../uploads/sections/outlet.png';
+import kitchenImg from '../../../../uploads/sections/cocina.png';
+import giftImg from '../../../../uploads/sections/gift.png';
+import beautyImg from '../../../../uploads/sections/beauty.png';
+
+export const sectionsData = [
+  {
+    title: 'Echo Show 10',
+    img: echoShowImg,
+    linkName: 'Ver más',
+    link: '',
+    isActive: true
+  },
+  {
+    title: 'Ayuda a la gente de Turquía y Siria',
+    img: helpImg,
+    linkName: 'Si puedes, colabora',
+    link: '',
+    isActive: true
+  },
+  {
+    title: 'Informática y accesorios',
+    img: informaticaImg,
+    linkName: 'Descubre más',
+    link: '',
+    isActive: true
+  },
+  {
+    title: 'Ofertas en Outlet',
+    img: outletImg,
+    linkName: 'Explora ahora',
+    link: '',
+    isActive: true
+  },
+  {
+    title: 'Ideas para Casa y cocina',
+    img: kitchenImg,
+    linkName: 'Saber más',
+    link: '',
+    isActive: true
+  },
+  {
+    title: '¿Buscas un regalo?',
+    img: giftImg,
+    linkName: 'Descubre algunas ideas',
+    link: '',
+    isActive: true
+  },
+  {
+    title: 'Lo mejor en belleza',
+    img: beautyImg,
+    linkName: 'Explora ahora',
+    link: '',
+    isActive: true
+  },
+  {
+    title: 'Todo para tu gato programador',
+    img: gatoProgramadorImg,
+    linkName: '¡A programar!',
+    link: 'https://www.google.com/search?q=gato+programador&sxsrf=AJOqlzWdjssiku06SEjCSG0F7I0Pym5ATg:1676560525810&source=lnms&tbm=isch&sa=X&ved=2ahUKEwi3_NbUqpr9AhVdUqQEHVCKAdAQ_AUoAXoECAEQAw&biw=2560&bih=1500&dpr=1',
+    isActive: true
+  }
+]
