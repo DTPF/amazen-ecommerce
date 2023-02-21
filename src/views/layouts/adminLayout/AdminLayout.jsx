f5c742484e0a17830336d3ebf7f05cd88596302e
David T. Pizarro Frick <davidpizarrofrick@gmail.com>
2023-02-21 11:53:44 +0100

    feat(admin): add protection

--- src/views/layouts/adminLayout/AdminLayout.jsx
+++ src/views/layouts/adminLayout/AdminLayout.jsx
@@ -1,21 +1,34 @@
-import React, { Suspense, lazy } from 'react';
+import React, { useEffect, Suspense, lazy } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { useGetUser } from '../../../providers/authProvider';
 import './AdminLayout.scss';
 const HeaderAdmin = lazy(() => import('../../components/admin/layout/headerAdmin/HeaderAdmin'));
 const FooterAdmin = lazy(() => import('../../components/admin/layout/footerAdmin/FooterAdmin'));
 const SubRoutes = lazy(() => import('../../../config/SubRoutes'));
 
 export default function AdminLayout({ routes }) {
+  const getUser = useGetUser();
+  let navigate = useNavigate();
+
+  useEffect(() => {
+    if (getUser && (getUser.role !== 'admin')) {
+      navigate('/');
+    }
+  }, [getUser, navigate]);
+
   return (
     <div className='admin-layout'>
-      <Suspense fallback={<></>}>
-        <HeaderAdmin />
+      {getUser.role === 'admin' && (
+        <Suspense fallback={<></>}>
+          <HeaderAdmin />
 
-        <div className='admin-layout__main'>
-          <SubRoutes routes={routes} /> {/* Switch views/pages/admin */}
-        </div>
+          <div className='admin-layout__main'>
+            <SubRoutes routes={routes} /> {/* Switch views/pages/admin */}
+          </div>
 
-        <FooterAdmin />
-      </Suspense>
+          <FooterAdmin />
+        </Suspense>
+      )}
     </div>
   );
 }
