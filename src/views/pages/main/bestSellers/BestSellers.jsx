e7a96f811599d67ae0984c1c66a377eb4603a101
David T. Pizarro Frick <davidpizarrofrick@gmail.com>
2023-02-22 20:16:16 +0100

    add articles component to Bestsellers page

--- src/views/pages/main/bestSellers/BestSellers.jsx
+++ src/views/pages/main/bestSellers/BestSellers.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Articles from '../../../components/main/desktop/articles/Articles';
 import HelmetSEO from "../../../utils/helmetSEO/HelmetSEO";
 import './BestSellers.scss';
 
@@ -7,6 +8,7 @@ export default function BestSellers() {
     <HelmetSEO title={'Amazén | Éxito de ventas'}>
       <div className='best-sellers'>
         <h1>Éxito de ventas</h1>
+        <Articles />
       </div>
     </HelmetSEO>
   );
