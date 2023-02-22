d79156cc103990f1b775209f679a874ea550af9d
David T. Pizarro Frick <davidpizarrofrick@gmail.com>
2023-02-22 19:45:00 +0100

    add providers

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
 import Routes from './routes/Routes';
 import indexedDB from './indexedDB';
+import { CartProvider } from './providers/CartProvider';
+import { UserProvider } from './providers/UserProvider';
 
 export default function App() {
   // Create indexedDb and insert first data
   indexedDB();
   // Switch views/layouts
   return (
-    <Routes />
+    <UserProvider>
+      <CartProvider>
+        <Routes />
+      </CartProvider>
+    </UserProvider>
   );
 }
