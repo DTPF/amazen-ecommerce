2795ad5906c54c8d4e7e270247677009ec137a93
David T. Pizarro Frick <davidpizarrofrick@gmail.com>
2023-02-22 11:07:23 +0100

    update: implement helmet component

--- src/views/pages/messages/commingSoon/CommingSoon.jsx
+++ src/views/pages/messages/commingSoon/CommingSoon.jsx
@@ -1,19 +1,22 @@
 import React from 'react';
+import HelmetSEO from "../../../utils/helmetSEO/HelmetSEO";
 import { Result } from 'antd';
 import commingSoonImage from '../../../../assets/images/comming-soon.png';
 import './CommingSoon.scss';
 
 export default function CommingSoon() {
   return (
-    <div className='comming-soon'>
-      <Result
-        icon={<img src={commingSoonImage} alt='Comming soon' />}
-        extra={
-          <button onClick={() => window.history.back()}>
-            Volver
-          </button>
-        }
-      />
-    </div>
+    <HelmetSEO title={'Amazén | Próximamente'}>
+      <div className='comming-soon'>
+        <Result
+          icon={<img src={commingSoonImage} alt='Comming soon' />}
+          extra={
+            <button onClick={() => window.history.back()}>
+              Volver
+            </button>
+          }
+        />
+      </div>
+    </HelmetSEO>
   );
 }
--- src/views/pages/messages/error/Error.jsx
+++ src/views/pages/messages/error/Error.jsx
@@ -1,20 +1,23 @@
 import React from 'react';
+import HelmetSEO from "../../../utils/helmetSEO/HelmetSEO";
 import { Result } from 'antd';
 import './Error.scss';
 
 export default function Error() {
   return (
-    <div className='error'>
-      <Result
-        status="404"
-        title="404"
-        subTitle="Lo sentimos, la página que buscas no existe."
-        extra={
-          <button onClick={() => window.history.back()}>
-            Volver
-          </button>
-        }
-      />
-    </div>
+    <HelmetSEO title={'Amazén | Error'}>
+      <div className='error'>
+        <Result
+          status="404"
+          title="404"
+          subTitle="Lo sentimos, la página que buscas no existe."
+          extra={
+            <button onClick={() => window.history.back()}>
+              Volver
+            </button>
+          }
+        />
+      </div>
+    </HelmetSEO>
   );
 }
