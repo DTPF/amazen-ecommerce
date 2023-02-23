f4e8adb92fdce387ae0864211f875ac1609795c5
David T. Pizarro Frick <davidpizarrofrick@gmail.com>
2023-02-23 12:08:42 +0100

    add auth object to IDB

--- src/indexedDB/utils/manageObjects.js
+++ src/indexedDB/utils/manageObjects.js
@@ -1,18 +1,21 @@
-import { USERS, SECTIONS, ARTICLES, DB_VERSION, LS_NAME } from "../config";
+import { USERS, SECTIONS, ARTICLES, AUTH, DB_VERSION, LS_NAME } from "../config";
 
 export function createObjects(db) {
   if (!db.objectStoreNames.contains(SECTIONS)) {
     db.createObjectStore(SECTIONS, { keyPath: 'id', autoIncrement: true });
-    localStorage.setItem(LS_NAME, DB_VERSION);
   }
 
   if (!db.objectStoreNames.contains(USERS)) {
     db.createObjectStore(USERS, { keyPath: 'id', autoIncrement: true });
-    localStorage.setItem(LS_NAME, DB_VERSION);
   }
 
   if (!db.objectStoreNames.contains(ARTICLES)) {
     db.createObjectStore(ARTICLES, { keyPath: 'id', autoIncrement: true });
-    localStorage.setItem(LS_NAME, DB_VERSION);
   }
+
+  if (!db.objectStoreNames.contains(AUTH)) {
+    db.createObjectStore(AUTH, { keyPath: 'id' });
+  }
+
+  localStorage.setItem(LS_NAME, DB_VERSION);
 }
