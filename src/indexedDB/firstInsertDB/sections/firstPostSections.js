8324990a31253b2265e97648568d4c644bfd5dc9
David T. Pizarro Frick <davidpizarrofrick@gmail.com>
2023-02-25 15:49:27 +0100

    change: sections content

--- src/indexedDB/firstInsertDB/sections/firstPostSections.js
+++ src/indexedDB/firstInsertDB/sections/firstPostSections.js
@@ -4,13 +4,13 @@ import { sectionsData } from "./sectionsData";
 export default function firstPostSections(vls, db) {
   if (vls === 1 || vls < db.version) {
     sectionsData.forEach((section, index) => {
-      const { title, img, linkName, link, isActive } = section;
-      postSections(db, title, img, linkName, link, isActive, index);
+      const { title, img, linkName, link, linkRouter, isActive } = section;
+      postSections(db, title, img, linkName, link, linkRouter, isActive, index);
     });
   }
 }
 
-function postSections(db, title, img, linkName, link, isActive, index) {
+function postSections(db, title, img, linkName, link, linkRouter, isActive, index) {
   let transaction = db.transaction([SECTIONS], "readwrite");
   let sections = transaction.objectStore(SECTIONS);
   let section = {
@@ -19,6 +19,7 @@ function postSections(db, title, img, linkName, link, isActive, index) {
     img: img,
     linkName: linkName,
     link: link,
+    linkRouter: linkRouter,
     isActive: isActive
   };
   let request = sections.put(section);
--- src/indexedDB/firstInsertDB/sections/sectionsData.js
+++ src/indexedDB/firstInsertDB/sections/sectionsData.js
@@ -9,52 +9,17 @@ import beautyImg from '../../../uploads/sections/beauty.png';
 
 export const sectionsData = [
   {
-    title: 'Echo Show 10',
+    title: 'Los más vendidos',
     img: echoShowImg,
     linkName: 'Ver más',
-    link: '',
+    linkRouter: '/bestsellers',
     isActive: true
   },
   {
     title: 'Ayuda a la gente de Turquía y Siria',
     img: helpImg,
     linkName: 'Si puedes, colabora',
-    link: '',
-    isActive: true
-  },
-  {
-    title: 'Informática y accesorios',
-    img: informaticaImg,
-    linkName: 'Descubre más',
-    link: '',
-    isActive: true
-  },
-  {
-    title: 'Ofertas en Outlet',
-    img: outletImg,
-    linkName: 'Explora ahora',
-    link: '',
-    isActive: true
-  },
-  {
-    title: 'Ideas para Casa y cocina',
-    img: kitchenImg,
-    linkName: 'Saber más',
-    link: '',
-    isActive: true
-  },
-  {
-    title: '¿Buscas un regalo?',
-    img: giftImg,
-    linkName: 'Descubre algunas ideas',
-    link: '',
-    isActive: true
-  },
-  {
-    title: 'Lo mejor en belleza',
-    img: beautyImg,
-    linkName: 'Explora ahora',
-    link: '',
+    link: 'https://www2.cruzroja.es/-/ayuda-victimas-terremoto-turquia',
     isActive: true
   },
   {
@@ -62,6 +27,41 @@ export const sectionsData = [
     img: gatoProgramadorImg,
     linkName: '¡A programar!',
     link: 'https://www.google.com/search?q=gato+programador&sxsrf=AJOqlzWdjssiku06SEjCSG0F7I0Pym5ATg:1676560525810&source=lnms&tbm=isch&sa=X&ved=2ahUKEwi3_NbUqpr9AhVdUqQEHVCKAdAQ_AUoAXoECAEQAw&biw=2560&bih=1500&dpr=1',
+    isActive: true
+  },
+  {
+    title: 'Amazén Basics',
+    img: informaticaImg,
+    linkName: 'Descubre más',
+    linkRouter: '/stores',
+    isActive: true
+  },
+  {
+    title: 'Atención al Cliente',
+    img: outletImg,
+    linkName: 'Explora ahora',
+    linkRouter: '/help',
+    isActive: true
+  },
+  {
+    title: 'Ideas para Casa y cocina',
+    img: kitchenImg,
+    linkName: 'Saber más',
+    linkRouter: '/comming-soon',
+    isActive: true
+  },
+  {
+    title: '¿Buscas un regalo?',
+    img: giftImg,
+    linkName: 'Descubre algunas ideas',
+    linkRouter: '/comming-soon',
+    isActive: true
+  },
+  {
+    title: 'Lo mejor en belleza',
+    img: beautyImg,
+    linkName: 'Explora ahora',
+    linkRouter: '/comming-soon',
     isActive: true
   }
 ]
