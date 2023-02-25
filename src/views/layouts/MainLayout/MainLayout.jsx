fbe4179e93688699fff4afdcc8481eb123f9027a
David T. Pizarro Frick <davidpizarrofrick@gmail.com>
2023-02-25 15:55:01 +0100

    change(MainLayout): add mobile components

--- src/views/layouts/MainLayout/MainLayout.jsx
+++ src/views/layouts/MainLayout/MainLayout.jsx
@@ -1,22 +1,39 @@
 import React, { Suspense, lazy } from "react";
+import useWindowSizeReport from "../../../hooks/useWindowSizeReport";
+import HeaderMainMobile from "../../components/main/mobile/layout/HeaderMainMobile";
+import NavBarMobile from "../../components/main/mobile/layout/NavBarMobile";
+import { responsiveBreak } from "../../utils/componentsConstants";
 import './MainLayout.scss';
-const HeaderMain = lazy(() => import('../../components/main/desktop/layout/HeaderMain'));
-const FooterMain = lazy(() => import('../../components/main/desktop/layout/FooterMain'));
+const HeaderMainDesktop = lazy(() => import('../../components/main/desktop/layout/HeaderMain'));
+const FooterMainDesktop = lazy(() => import('../../components/main/desktop/layout/FooterMain'));
 const MenuMain = lazy(() => import('../../components/main/desktop/layout/MenuMain'));
 const SubRoutes = lazy(() => import('../../../routes/SubRoutes'));
 
+const FooterMainMobile = lazy(() => import('../../components/main/mobile/layout/FooterMainMobile'));
+
 export default function MainLayout({ routes }) {
+  const innerWidth = useWindowSizeReport();
+
   return (
     <div className='main-layout'>
       <Suspense fallback={<></>}>
-        <HeaderMain />
-        <MenuMain />
+        {(innerWidth > responsiveBreak) ? (
+          <>
+            <HeaderMainDesktop />
+            <MenuMain />
+          </>
+        ) : (
+          <>
+            <HeaderMainMobile />
+            <NavBarMobile />
+          </>
+        )}
 
         <div className='main-layout__main'>
           <SubRoutes routes={routes} /> {/* Switch views/pages/main */}
         </div>
 
-        <FooterMain />
+        {(innerWidth > responsiveBreak) ? <FooterMainDesktop /> : <FooterMainMobile />}
       </Suspense>
     </div>
   );
