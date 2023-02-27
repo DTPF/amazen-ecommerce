3d4b4283802be6889a858c712faafd671c7527a9
David T. Pizarro Frick <davidpizarrofrick@gmail.com>
2023-02-27 14:38:41 +0100

    feat(activePage): add active component

---
+++ src/views/pages/ActivePage/ActivePage.jsx
@@ -0,0 +1,6 @@
+import React from 'react'
+import WishlistComponent from '../../components/WishlistComponent';
+
+export default function ActivePage() {
+  return <WishlistComponent status={'active'} />;
+}
