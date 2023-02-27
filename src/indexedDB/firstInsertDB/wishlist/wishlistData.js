d2e368fb1f7d75e84d0debdcb30a61303b39f268
David T. Pizarro Frick <davidpizarrofrick@gmail.com>
2023-02-27 14:33:22 +0100

    change(indexedDb): add new data

--- src/indexedDB/firstInsertDB/wishlist/wishlistData.js
+++ src/indexedDB/firstInsertDB/wishlist/wishlistData.js
@@ -27,5 +27,34 @@ export const wishlistData = [
     title: 'Cangrejos de río',
     status: 'completed',
     createdAt: Date.now()
+  },
+  {
+    id: 5,
+    userId: 'd@mail.com',
+    title: 'JETech Funda para iPhone 13 6,1 Pulgadas, Anti-Amarillo Carcasa de Parachoques Prueba Golpes',
+    link: '/comming-soon',
+    status: 'active',
+    createdAt: Date.now()
+  },
+  {
+    id: 6,
+    userId: 'd@mail.com',
+    title: 'Junta de la trócola',
+    status: 'active',
+    createdAt: Date.now()
+  },
+  {
+    id: 7,
+    userId: 'd@mail.com',
+    title: 'Cangrejos de río',
+    status: 'completed',
+    createdAt: Date.now()
+  },
+  {
+    id: 8,
+    userId: 'm@mail.com',
+    title: 'Cangrejos de río',
+    status: 'completed',
+    createdAt: Date.now()
   }
 ]
