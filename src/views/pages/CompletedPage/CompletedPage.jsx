a64e7ccaee632f15d512eed1a7f804422bec6dd0
David T. Pizarro Frick <davidpizarrofrick@gmail.com>
2023-02-27 14:39:03 +0100

    feat(completedPage): add completed component

---
+++ src/views/pages/CompletedPage/CompletedPage.jsx
@@ -0,0 +1,6 @@
+import React from 'react'
+import WishlistComponent from '../../components/WishlistComponent';
+
+export default function CompletedPage() {
+  return <WishlistComponent status={'completed'} />;
+}
