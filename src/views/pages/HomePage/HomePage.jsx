9250a5478119792851ab55aef7a0f24cbb23cfe6
David T. Pizarro Frick <davidpizarrofrick@gmail.com>
2023-02-27 14:38:21 +0100

    feat(homePage): add page component

---
+++ src/views/pages/HomePage/HomePage.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import WishlistComponent from '../../components/WishlistComponent';
+
+export default function HomePage() {
+  return <WishlistComponent status={''} />;
+}
