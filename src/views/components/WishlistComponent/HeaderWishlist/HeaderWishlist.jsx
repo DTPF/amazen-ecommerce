dbe4a9ac3a5c7f8e7d6fe10410c64f206738c4dc
David T. Pizarro Frick <davidpizarrofrick@gmail.com>
2023-02-28 14:13:29 +0100

    feat(header): add component with nav

--- src/views/components/WishlistComponent/HeaderWishlist/HeaderWishlist.jsx
+++ src/views/components/WishlistComponent/HeaderWishlist/HeaderWishlist.jsx
@@ -1,18 +1,37 @@
-import React from 'react'
-import { Link } from 'react-router-dom';
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+import { useWishlistContext } from '../../../../providers/WishlistProvider';
+import './HeaderWishlist.scss';
 
 export default function HeaderWishlist() {
+   const { wishlist } = useWishlistContext();
+
   return (
-    <div>
-      <Link to={'/'}>
-        <button>Todo</button>
-      </Link>
-      <Link to={'/active'}>
-        <button>Activo</button>
-      </Link>
-      <Link to={'/completed'}>
-        <button>Completado</button>
-      </Link>
+    <div className='header-wishlist'>
+      <div className='header-wishlist__title'>
+        <h1>Wishlist</h1>
+      </div>
+
+      <div className='header-wishlist__nav'>
+        <NavLink to={'/'}>
+          <button className='header-wishlist__nav--button'>
+            Todo
+            <div className='header-wishlist__nav--button__items-count'>{wishlist.length}</div>
+          </button>
+        </NavLink>
+        <NavLink to={'/active'}>
+          <button className='header-wishlist__nav--button'>
+            Activo
+            <div className='header-wishlist__nav--button__items-count'>{wishlist.length}</div>
+          </button>
+        </NavLink>
+        <NavLink to={'/completed'}>
+          <button className='header-wishlist__nav--button'>
+            Completado
+            <div className='header-wishlist__nav--button__items-count'>{wishlist.length}</div>
+          </button>
+        </NavLink>
+      </div>
     </div>
   )
 }
