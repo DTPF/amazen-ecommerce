77ce2c9e15da0235daa2be85e3fdc650746b9a06
David T. Pizarro Frick <davidpizarrofrick@gmail.com>
2023-02-28 14:17:50 +0100

    feat(title): add component

---
+++ src/views/components/WishlistComponent/TitleWishlist_U/TitleWishlist.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './TitleWishlist.scss';
+
+export default function TitleWishlist() {
+  return (
+    <div className='title-wishlist'>
+      <h1>Whislist</h1>
+    </div>
+  )
+}
