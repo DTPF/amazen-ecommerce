bfb8d7d3872ff0c5fac4f9f98354d9bb43ab52a6
David T. Pizarro Frick <davidpizarrofrick@gmail.com>
2023-03-01 12:51:25 +0100

    feat(bottomList): add functionality to remove completed items

--- src/views/components/WishlistComponent/BottomList/BottomList.jsx
+++ src/views/components/WishlistComponent/BottomList/BottomList.jsx
@@ -1,31 +1,74 @@
 import React, { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import { useWishlistContext } from '../../../../providers/WishlistProvider';
+import { DB_NAME_AMAZEN, DB_VERSION, WISHLIST } from '../../../../indexedDB/config';
 import './BottomList.scss';
 
 export default function BottomList() {
-  const { wishlist } = useWishlistContext();
-  const [hasCompleted, setHasCompleted] = useState(false);
+  const { wishlist, setWishlist } = useWishlistContext();
+  const [hasCompletedItems, setHasCompletedItems] = useState(false);
+  const navigate = useNavigate();
 
   useEffect(() => {
+    let isMounted = true;
     wishlist && wishlist.forEach(item => {
       if (item.status === 'active') {
-        setHasCompleted(false)
+        isMounted && setHasCompletedItems(false);
       }
     });
-  }, [wishlist])
+    return () => { isMounted = false }
+  }, [wishlist]);
 
   useEffect(() => {
+    let isMounted = true;
     wishlist && wishlist.forEach(item => {
       if (item.status === 'completed') {
-        setHasCompleted(true)
+        isMounted && setHasCompletedItems(true);
       }
     });
-  }, [wishlist])
+    return () => { isMounted = false }
+  }, [wishlist]);
+
+  const handleClickDelete = () => {
+    let openRequest = indexedDB.open(DB_NAME_AMAZEN, DB_VERSION);
+
+    openRequest.onsuccess = function (e) {
+      let db = e.target.result;
+      let transaction = db.transaction([WISHLIST], "readwrite");
+      let listItem = transaction.objectStore(WISHLIST);
+      let request = listItem.getAll();
+
+      request.onsuccess = function (e) {
+        let result = e.target.result;
+
+        result.forEach((item) => {
+          if (item.status === 'completed') {
+            listItem.delete(item.id);
+            let request = listItem.getAll();
+
+            request.onsuccess = function (e) {
+              const result = e.target.result;
+              setWishlist(result);
+            }
+          }
+        });
+        window.location.pathname === '/completed' && navigate('/');
+      };
+
+      request.onerror = function () {
+        console.log('Request error');
+      };
+    }
+
+    openRequest.onerror = function () {
+      console.log('Server error');
+    }
+  }
 
   return (
     <div className='bottom-list'>
-      {hasCompleted && (
-        <p className='bottom-list__delete-completed'>
+      {hasCompletedItems && (
+        <p className='bottom-list__delete-completed' onClick={() => handleClickDelete()}>
           Borrar completado
         </p>
       )}
