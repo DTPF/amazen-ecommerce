6b280e88c7ae8225a844765ed551bd16dfa1f925
David T. Pizarro Frick <davidpizarrofrick@gmail.com>
2023-03-03 13:12:16 +0100

    change(app.js): remove user provider and add auth provider

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,17 @@
 import Routes from './routes/Routes';
 import indexedDB from './indexedDB';
 import { CartProvider } from './providers/CartProvider';
-import { UserProvider } from './providers/UserProvider';
+import AuthProvider from './providers/AuthProvider';
 
 export default function App() {
   // Create indexedDb and insert first data
   indexedDB();
   // Switch views/layouts
   return (
-    <UserProvider>
+    <AuthProvider>
       <CartProvider>
         <Routes />
       </CartProvider>
-    </UserProvider>
+    </AuthProvider>
   );
 }
