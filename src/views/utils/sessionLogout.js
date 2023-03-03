ddc5b103d333e692188329cecf9a65ed9771a379
David T. Pizarro Frick <davidpizarrofrick@gmail.com>
2023-03-03 13:24:41 +0100

    remove(indexeDB): unused function

--- src/views/utils/sessionLogout.js
+++
@@ -1,8 +0,0 @@
-import deleteUserIDB from "../../indexedDB/api/auth/deleteUserIDB";
-
-export default async function handleSessionLogout() {
-  await deleteUserIDB().then(() => {
-    localStorage.clear();
-    window.location.reload();
-  });
-}
