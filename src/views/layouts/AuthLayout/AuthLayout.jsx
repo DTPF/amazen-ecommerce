0dc25c4f9ed21912ded1459c12b31036f1469406
David T. Pizarro Frick <davidpizarrofrick@gmail.com>
2023-03-03 17:55:42 +0100

    change(auth): change indexedDB for api rest provider

--- src/views/layouts/AuthLayout/AuthLayout.jsx
+++ src/views/layouts/AuthLayout/AuthLayout.jsx
@@ -1,25 +1,32 @@
 import React, { useEffect, Suspense, lazy } from "react";
 import { useNavigate } from 'react-router-dom';
-import useUserContext from "../../../hooks/useUserContext";
+import useAuth from "../../../hooks/useAuth";
 const SubRoutes = lazy(() => import('../../../routes/SubRoutes'));
 const FooterAuth = lazy(() => import('../../components/auth/layout/FooterAuth'));
 
 export default function AuthLayout({ routes }) {
-  const { userContext } = useUserContext();
+  const { user, setUser } = useAuth();
+  const { userData, isLoading } = user;
   let navigate = useNavigate();
 
   useEffect(() => {
-    userContext && navigate('/');
-  }, [userContext, navigate]);
+    setUser({
+      isLoading: false,
+      userData: null
+    })
+  }, [setUser]);
+
+  useEffect(() => {
+    userData && !isLoading && navigate('/');
+  }, [userData, isLoading, navigate]);
 
   return (
     <div className='auth-layout'>
-      {!userContext && (
+      {(!userData && !isLoading) && (
         <Suspense fallback={<></>}>
           <div className='auth-layout__main'>
             <SubRoutes routes={routes} /> {/* Switch views/pages/main */}
           </div>
-
           <FooterAuth />
         </Suspense>
       )}
