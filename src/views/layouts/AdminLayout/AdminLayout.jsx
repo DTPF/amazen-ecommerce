a09f90a7f722dba2905084b64230b38eadbca16b
David T. Pizarro Frick <davidpizarrofrick@gmail.com>
2023-03-03 17:55:16 +0100

    change(admin): change indexedDB for api rest provider

--- src/views/layouts/AdminLayout/AdminLayout.jsx
+++ src/views/layouts/AdminLayout/AdminLayout.jsx
@@ -1,34 +1,35 @@
 import React, { useEffect, Suspense, lazy } from 'react';
 import { useNavigate } from 'react-router-dom';
-// import useGetUserFromIndexedDB from '../../../hooks/users/useGetUserFromIndexedDB';
-import useUserContext from '../../../hooks/useUserContext';
+import useAuth from '../../../hooks/useAuth';
 import './AdminLayout.scss';
 const HeaderAdmin = lazy(() => import('../../components/admin/layout/HeaderAdmin/HeaderAdmin'));
 const FooterAdmin = lazy(() => import('../../components/admin/layout/FooterAdmin'));
 const SubRoutes = lazy(() => import('../../../routes/SubRoutes'));
 
 export default function AdminLayout({ routes }) {
-  const { userContext } = useUserContext();
-  // const userIDB = useGetUserFromIndexedDB(userContext.id);
+  const { user } = useAuth();
+  const { userData, isLoading } = user;
   const navigate = useNavigate();
 
   useEffect(() => {
-    userContext?.role !== 'admin' && navigate('/');
-  }, [userContext, navigate]);
+    !userData && !isLoading && navigate('/');
+  }, [userData, isLoading, navigate]);
 
-  return (
-    <div className='admin-layout'>
-      {userContext?.role === 'admin' && (
+  useEffect(() => {
+    (userData?.role !== 'admin') && !isLoading && navigate('/');
+  }, [userData, isLoading, navigate]);
+
+  if (userData?.role === 'admin') {
+    return (
+      <div className='admin-layout'>
         <Suspense fallback={<></>}>
           <HeaderAdmin />
-
           <div className='admin-layout__main'>
             <SubRoutes routes={routes} /> {/* Switch views/pages/admin */}
           </div>
-
           <FooterAdmin />
         </Suspense>
-      )}
-    </div>
-  );
+      </div>
+    );
+  }
 }
