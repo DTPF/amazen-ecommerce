bba53b29f0afff9afd1d867c7703aff7ed1a308f
David T. Pizarro Frick <davidpizarrofrick@gmail.com>
2023-03-03 18:21:42 +0100

    remove: unnecesary code

--- src/hooks/useGetUserFromIndexedDB.js
+++
@@ -1,7 +0,0 @@
-import useGetUserById from "../indexedDB/api/users/useGetUserById";
-
-export default function useGetUserFromIndexedDB(id) {
-  const getUser = useGetUserById(id);
-
-  return getUser;
-}
