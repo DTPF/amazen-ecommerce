2c183e7be56840ecada6836e4dd5ba8ac58851a4
David T. Pizarro Frick <davidpizarrofrick@gmail.com>
2023-03-03 16:46:35 +0100

    change(FooterMobile): change user provider for auth provider

--- src/views/components/main/mobile/layout/FooterMainMobile/FooterMainMobile.jsx
+++ src/views/components/main/mobile/layout/FooterMainMobile/FooterMainMobile.jsx
@@ -1,26 +1,23 @@
 import React from 'react';
 import { NavLink, Link } from 'react-router-dom';
-import useUserContext from '../../../../../../hooks/useUserContext';
-import handleSessionLogout from '../../../../../utils/sessionLogout';
+import useAuth from '../../../../../../hooks/useAuth';
 import useSetCartCount from '../../../../../../hooks/useSetCartCount';
-import useGetUserFromIndexedDB from '../../../../../../hooks/useGetUserFromIndexedDB';
 import { AiOutlineUser, AiOutlineHome, AiOutlineStar } from 'react-icons/ai';
 import { MdExitToApp } from 'react-icons/md';
 import { RiAdminLine } from 'react-icons/ri';
 import { BiCart } from 'react-icons/bi';
 import './FooterMainMobile.scss';
 
-
 export default function FooterMainMobile() {
-  const { userContext } = useUserContext();
-  const userIdb = useGetUserFromIndexedDB(userContext?.id);
+  const { user } = useAuth();
+  const { userData } = user;
   const cartCount = useSetCartCount();
   
   return (
     <div className='footer-main-mobile'>
       <NavLink to={'/'}><AiOutlineHome /></NavLink>
       <NavLink to={'/comming-soon'}><AiOutlineStar /></NavLink>
-      {userContext && (
+      {userData && (
         <div className='footer-main-mobile__cart'>
           <span>{cartCount}</span>
           <NavLink to={'/comming-soon'}>
@@ -28,16 +25,16 @@ export default function FooterMainMobile() {
           </NavLink>
         </div>
       )}
-      {userContext ? (
+      {userData ? (
         <Link>
-          <MdExitToApp onClick={() => handleSessionLogout()} />
+          <MdExitToApp onClick={() => console.log('logout')} />
         </Link>
       ) : (
         <NavLink to={'/auth'}>
           <AiOutlineUser />
         </NavLink>
       )}
-      {userIdb.role === 'admin' && (
+      {userData?.role === 'admin' && (
         <NavLink to={'/admin'}>
           <RiAdminLine />
         </NavLink>
