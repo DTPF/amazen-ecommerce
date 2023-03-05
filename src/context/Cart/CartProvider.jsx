415599115fe3877f5dfa50bb33bef17091120c98
David T. Pizarro Frick <davidpizarrofrick@gmail.com>
2023-03-05 12:32:36 +0100

    change(cartProvider): add auth middleware

--- src/context/Cart/CartProvider.jsx
+++ src/context/Cart/CartProvider.jsx
@@ -1,23 +1,29 @@
 import React, { useEffect, useState } from 'react';
 import { CartContext } from './CartContext';
-import { getCartItemsByUserId } from '../../api/cart';
 import useAuth from '../../hooks/useAuth';
+import { useGetAccessTokenApi } from '../../api/auth';
+import { getCartItemsByUserId } from '../../api/cart';
+import toaster from '../../views/components/UI/toast/toast';
 
 export default function CartProvider({ children }) {
   const [cart, setCart] = useState([]);
   const { user } = useAuth();
   const { userData } = user;
+  const token = useGetAccessTokenApi();
 
   useEffect(() => {
     let isMounted = true;
     if (userData) {
-      getCartItemsByUserId(userData.id)
+      getCartItemsByUserId(token, userData.id)
         .then(res => {
           isMounted && setCart(res.cartItems)
+        })
+        .catch(err => {
+          toaster('Error al obtener el carrito', 'error');
         });
     }
     return () => { isMounted = false }
-  }, [userData]);
+  }, [userData, token]);
 
   return (
     <CartContext.Provider value={{ cart, setCart }} >
