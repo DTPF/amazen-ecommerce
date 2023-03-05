1bbb8a1d3c1598882457888e904796573f5f6664
David T. Pizarro Frick <davidpizarrofrick@gmail.com>
2023-03-05 11:44:13 +0100

    change(cart): rename cart

--- src/hooks/useCart.js
+++ src/hooks/useCart.js
@@ -1,7 +1,7 @@
 import { useContext } from 'react';
 import { CartContext } from '../context/Cart/CartContext';
 
-export default function useCartContext() {
+export default function useCart() {
   const context = useContext(CartContext);
   if (!context) {
     throw new Error('useUserContext must be used within a UserContextProvider');
