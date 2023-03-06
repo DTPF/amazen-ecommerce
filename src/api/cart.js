89dd43328ba68649951662262735b22f43f60c44
David T. Pizarro Frick <davidpizarrofrick@gmail.com>
2023-03-06 10:06:22 +0100

    change(cart): add deleteCartItemApi and put token to others api's

--- src/api/cart.js
+++ src/api/cart.js
@@ -1,12 +1,17 @@
 import { basePath, apiVersion } from "./utils/config";
 import makeRequest from "./utils/makeRequest";
 
-export async function addToCart(data) {
+export async function addToCart(token, data) {
   const url = `${basePath}/${apiVersion}/add-to-cart`;
-  return makeRequest(url, true, true, "POST", null, JSON.stringify(data));
+  return makeRequest(url, true, true, "POST", token, JSON.stringify(data));
 }
 
-export async function getCartItemsByUserId(id) {
+export async function getCartItemsByUserId(token, id) {
   const url = `${basePath}/${apiVersion}/get-cart-items-by-user-id/${id}`;
-  return makeRequest(url, true, true, "GET");
+  return makeRequest(url, true, true, "GET", token);
+}
+
+export async function deleteCartItemApi(token, cartId) {
+  const url = `${basePath}/${apiVersion}/delete-cart-item-by-id/${cartId}`;
+  return makeRequest(url, true, true, "DELETE", token);
 }
