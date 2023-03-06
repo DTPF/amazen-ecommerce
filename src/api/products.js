ff77baf4569166af8b75a15e6a925420c34b5e91
David T. Pizarro Frick <davidpizarrofrick@gmail.com>
2023-03-06 10:08:56 +0100

    change(product): add endpoints

--- src/api/products.js
+++ src/api/products.js
@@ -1,7 +1,22 @@
 import { basePath, apiVersion } from "./utils/config";
 import makeRequest from "./utils/makeRequest";
 
-export async function getProducts() {
+export async function getProductsApi() {
   const url = `${basePath}/${apiVersion}/get-products`;
   return makeRequest(url, true, true, "GET");
+}
+
+export async function getProductByIdApi(id) {
+  const url = `${basePath}/${apiVersion}/get-product-by-id/${id}`;
+  return makeRequest(url, true, true, "GET");
+}
+
+export function getProductImageApi(imageName) {
+  const url = `${basePath}/${apiVersion}/get-product-image/${imageName}`;
+  return makeRequest(url, null, false);
+}
+
+export function deleteProductApi(userId) {
+  const url = `${basePath}/${apiVersion}/delete-product/${userId}`;
+  return makeRequest(url, true, true, "DELETE");
 }
