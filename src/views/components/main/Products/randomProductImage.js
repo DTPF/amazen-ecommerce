8f0ef4c35a4285677780ad5f6415d7a185a95b10
David T. Pizarro Frick <davidpizarrofrick@gmail.com>
2023-03-06 23:29:12 +0100

    rename(product): randomImage function

--- src/views/components/main/Products/randomProductImage.js
+++ src/views/components/main/Products/randomProductImage.js
@@ -1,7 +1,7 @@
 import product1 from '../../../../uploads/products/product-1.png';
 import product2 from '../../../../uploads/products/product-2.png';
 
-export default function randomAvatar() {
+export default function randomProductImage() {
   const images = [product1, product2];
   const random = Math.floor(Math.random() * images.length);
 
