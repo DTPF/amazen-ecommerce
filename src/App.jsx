ba11dd5c102dd0902a77010d10157770ca5b7a69
David T. Pizarro Frick <davidpizarrofrick@gmail.com>
2023-03-07 09:54:25 +0100

    change: rename import

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { RouterProvider } from "react-router-dom";
-import { router } from './routes/routes';
+import router from './router/router';
 import indexedDB from './indexedDB';
 import AuthProvider from './context/Auth/AuthProvider';
 import ProductProvider from './context/Product/ProductProvider';
