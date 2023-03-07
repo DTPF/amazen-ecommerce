abc601ff05075c2de750dc48648ee5f42b48367b
David T. Pizarro Frick <davidpizarrofrick@gmail.com>
2023-03-07 10:33:42 +0100

    feat(logout): add function to logout

---
+++ src/hooks/useUser/userLogout.js
@@ -0,0 +1,13 @@
+import { logout } from '../../api/auth';
+import toaster from '../../views/components/UI/toast/toast';
+
+export default function userLogout(setUser, setCart) {
+  logout().then(() => {
+    setUser({
+      isLoading: false,
+      userData: null,
+    });
+    setCart(0)
+    toaster('¡Hasta pronto!');
+  });
+}
