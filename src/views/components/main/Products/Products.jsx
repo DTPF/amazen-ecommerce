fdeb94cf87d87e302afb1892c3812d2f89ba4145
David T. Pizarro Frick <davidpizarrofrick@gmail.com>
2023-03-07 11:57:26 +0100

    change(product): get data from API

--- src/views/components/main/Products/Products.jsx
+++ src/views/components/main/Products/Products.jsx
@@ -1,18 +1,18 @@
-import React, { useContext } from 'react';
-import useAuth from '../../../../hooks/useAuth';
+import React from 'react';
+import useCartContext from '../../../../hooks/useCartContext';
+import useProductContext from '../../../../hooks/useProductContext';
+import useAuth from '../../../../hooks/useAuthContext';
 import { useGetAccessTokenApi } from '../../../../api/auth';
-import { CartContext } from '../../../../context/Cart/CartContext';
-import useProduct from '../../../../hooks/useProduct';
 import { addToCart } from '../../../../api/cart';
+import useGetProductImage from '../../../../hooks/useProduct/useGetProductImage';
 import toaster from '../../UI/toast/toast';
-import randomAvatar from './ramdomProductImage';
+import randomProductImage from './randomProductImage';
 import fiveStarsImage from '../../../../assets/images/five-stars.png';
 import { CgShoppingCart } from 'react-icons/cg';
 import './Products.scss';
 
-
 export default function Products() {
-  const { products } = useProduct();
+  const { products } = useProductContext();
 
   return (
     <div className='products-container'>
@@ -20,11 +20,8 @@ export default function Products() {
         {products && products.map((product, index) => (
           <ProductsRender
             key={index}
-            id={product._id}
-            title={product.title}
-            image={product.image ? product.image : randomAvatar()}
-            stars={product.stars}
-            price={product.sizeAndPrice[0].price}
+            imageRandom={product.image ? product.image : randomProductImage()}
+            product={product}
           />
         ))}
       </div>
@@ -32,17 +29,19 @@ export default function Products() {
   );
 }
 
-function ProductsRender(props) {
-  const { id, title, image, stars, price } = props;
+function ProductsRender({ imageRandom, product }) {
+  const { _id, title, stars, sizeAndPrice } = product;
   const { user } = useAuth();
   const { userData } = user;
-  const { cart, setCart } = useContext(CartContext);
+  const { cart, setCart } = useCartContext();
+  const [image] = useGetProductImage(product)
   const token = useGetAccessTokenApi();
 
   const handleAddToCart = async () => {
     const productObj = {
       userId: userData.id,
-      productId: id
+      productId: _id,
+      price: sizeAndPrice[0].price
     }
 
     await addToCart(token, productObj)
@@ -52,7 +51,7 @@ function ProductsRender(props) {
           let isRepeated = false;
 
           list.forEach((item, key) => {
-            if (item.productId === id.toString()) {
+            if (item.productId === _id) {
               list[key].quantity = product.quantity;
               isRepeated = true;
               return;
@@ -72,18 +71,23 @@ function ProductsRender(props) {
 
   return (
     <div className='products-container__products__article'>
-      <img className='products-container__products__article--image' src={image} alt={title} />
-      <h3 className='products-container__products__article--title'>{title}</h3>
-      <p className='products-container__products__article--stars'>
-        <img src={fiveStarsImage} alt='Five stars' />
-        {stars}
-      </p>
-      <p className='products-container__products__article--price'>{price} €</p>
-      {userData && (
-        <div className='products-container__products__article--add-to-cart'>
-          <button onClick={() => handleAddToCart()}><CgShoppingCart /></button>
-        </div>
-      )}
+      <div className='products-container__products__article--image'>
+        <img src={image ? image : imageRandom} alt={title} />
+      </div>
+      <div>
+
+        <h3 className='products-container__products__article--title'>{title}...</h3>
+        <p className='products-container__products__article--stars'>
+          <img src={fiveStarsImage} alt='Five stars' />
+          {stars}
+        </p>
+        <p className='products-container__products__article--price'>{sizeAndPrice[0].price} €</p>
+        {userData && (
+          <div className='products-container__products__article--add-to-cart'>
+            <button onClick={() => handleAddToCart()}><CgShoppingCart /></button>
+          </div>
+        )}
+      </div>
     </div>
   )
 }
