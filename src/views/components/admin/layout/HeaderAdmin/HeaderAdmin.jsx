b9b8126c6fdfd433b7416acad0cb0c506fd30635
David T. Pizarro Frick <davidpizarrofrick@gmail.com>
2023-03-08 10:17:44 +0100

    change(addAnchor): add anchor and prime image

--- src/views/components/admin/layout/HeaderAdmin/HeaderAdmin.jsx
+++ src/views/components/admin/layout/HeaderAdmin/HeaderAdmin.jsx
@@ -1,11 +1,18 @@
 import React from 'react';
-import logoAdmin from '../../../../../assets/images/amazen-logo-admin.png'
+import { Link } from 'react-router-dom';
+import logoAdmin from '../../../../../assets/images/amazen-logo-admin.png';
+import primeImage from '../../../../../assets/images/prime.png';
 import './HeaderAdmin.scss';
 
 export default function HeaderAdmin(props) {
   return (
     <div className='header-admin'>
-      <img className='header-admin__logo' src={logoAdmin} alt='Admin logo' />
+      <Link to={'/admin/home'}>
+        <img className='header-admin__logo' src={logoAdmin} alt='Admin logo' />
+      </Link>
+      <Link to={'/'}>
+        <img className='header-admin__prime' src={primeImage} alt='Admin prime' />
+      </Link>
     </div>
   );
 }
