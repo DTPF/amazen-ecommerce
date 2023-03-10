45d0e52273f9bfccebd12219f566ea16a0180099
David T. Pizarro Frick <davidpizarrofrick@gmail.com>
2023-03-10 17:05:13 +0100

    feat(addProduct): add component

---
+++ src/views/components/admin/ProductsAdmin/AddProduct/AddProduct.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import ProductForm from '../ProductForm';
+import '../AddEditProductModal.scss';
+
+export default function AddProduct({ product, setIsVisibleAddModal }) {
+  return (
+    <div id="myModal" className="modal">
+      <div className="modal__modal-content">
+        <span onClick={() => setIsVisibleAddModal(false)} className="modal__modal-content--close">&times;</span>
+        <h3>Añadir un nuevo producto</h3>
+        <ProductForm product={product} setIsVisibleAddModal={setIsVisibleAddModal} />
+      </div>
+    </div>
+  )
+}
