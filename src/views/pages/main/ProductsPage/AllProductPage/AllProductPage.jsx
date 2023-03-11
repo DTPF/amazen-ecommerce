bc521daef39ce06f3684d5485bc9d46866a11b56
David T. Pizarro Frick <davidpizarrofrick@gmail.com>
2023-03-11 13:53:14 +0100

    refacto(productsTitle): pass the title by props

--- src/views/pages/main/ProductsPage/AllProductPage/AllProductPage.jsx
+++ src/views/pages/main/ProductsPage/AllProductPage/AllProductPage.jsx
@@ -1,15 +1,11 @@
 import React from 'react';
 import Products from '../../../../components/main/Products';
 import HelmetSEO from "../../../../utils/HelmetSEO/HelmetSEO";
-import './AllProductPage.scss';
 
 export default function AllProductPage() {
   return (
     <HelmetSEO title={'Amazén | Todos los productos'}>
-      <div className='all-products-page'>
-        <h1>Todos los productos</h1>
-        <Products category={'all'} />
-      </div>
+      <Products category={'all'} title={'Todos los productos'} />
     </HelmetSEO>
   );
 }
--- src/views/pages/main/ProductsPage/TravelPage/TravelPage.jsx
+++ src/views/pages/main/ProductsPage/TravelPage/TravelPage.jsx
@@ -1,15 +1,11 @@
 import React from 'react';
 import Products from '../../../../components/main/Products';
 import HelmetSEO from "../../../../utils/HelmetSEO/HelmetSEO";
-import './TravelPage.scss';
 
 export default function TravelPage() {
   return (
     <HelmetSEO title={'Amazén | Viajes'}>
-      <div className='travel-page'>
-        <h1>Productos para viajar</h1>
-        <Products category={'travel'} />
-      </div>
+      <Products category={'travel'} title={'Productos para viajar'} />
     </HelmetSEO>
   );
 }
