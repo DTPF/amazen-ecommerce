40b6d7582e461ed3464102b55982ef12bbc2fc7e
David T. Pizarro Frick <davidpizarrofrick@gmail.com>
2023-03-12 17:55:45 +0100

    remove(suspense)

--- src/views/layouts/MainLayout/MainLayout.jsx
+++ src/views/layouts/MainLayout/MainLayout.jsx
@@ -1,23 +1,22 @@
-import React, { Suspense, lazy } from "react";
+import React from "react";
 import { Outlet } from "react-router-dom";
 import useWindowSizeReport from "../../../hooks/useWindowSizeReport";
 import { responsiveBreak } from "../../utils/componentsConstants";
 import './MainLayout.scss';
 // Desktop
-const HeaderMainDesktop = lazy(() => import('../../components/main/desktop/layout/HeaderMain'));
-const FooterMainDesktop = lazy(() => import('../../components/main/desktop/layout/FooterMain'));
-const MenuMain = lazy(() => import('../../components/main/desktop/layout/MenuMain'));
+import HeaderMainDesktop from '../../components/main/desktop/layout/HeaderMain';
+import FooterMainDesktop from '../../components/main/desktop/layout/FooterMain';
+import MenuMain from '../../components/main/desktop/layout/MenuMain';
 //Mobile
-const HeaderMainMobile = lazy(() => import('../../components/main/mobile/layout/HeaderMainMobile'));
-const FooterMainMobile = lazy(() => import('../../components/main/mobile/layout/FooterMainMobile'));
-const NavBarMobile = lazy(() => import('../../components/main/mobile/layout/NavBarMobile'));
+import HeaderMainMobile from '../../components/main/mobile/layout/HeaderMainMobile';
+import FooterMainMobile from '../../components/main/mobile/layout/FooterMainMobile';
+import NavBarMobile from '../../components/main/mobile/layout/NavBarMobile';
 
-export default function MainLayout(props) {
+export default function MainLayout() {
   const [innerWidth] = useWindowSizeReport();
 
   return (
     <div className='main-layout'>
-      <Suspense fallback={<></>}>
         {(innerWidth > responsiveBreak) ? (
           <>
             <HeaderMainDesktop />
@@ -35,7 +34,6 @@ export default function MainLayout(props) {
         </div>
 
         {(innerWidth > responsiveBreak) ? <FooterMainDesktop /> : <FooterMainMobile />}
-      </Suspense>
     </div>
   );
 }
