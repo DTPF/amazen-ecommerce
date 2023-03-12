fd7f5bafe2d4a4ccb6a79ed872b1eda794740a34
David T. Pizarro Frick <davidpizarrofrick@gmail.com>
2023-03-12 17:53:27 +0100

    feat(order): add checkout page

---
+++ src/views/pages/main/Checkout/Checkout.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import CheckoutComponent from '../../../components/main/CheckoutComponent';
+import HelmetSEO from "../../../utils/HelmetSEO/HelmetSEO";
+import './Checkout.scss';
+
+export default function Checkout() {
+  return (
+    <HelmetSEO title={'Amazén | Realizar pedido'}>
+      <div className='checkout'>
+        <h1>Confirma los datos</h1>
+        <CheckoutComponent />
+      </div>
+    </HelmetSEO>
+  );
+}
