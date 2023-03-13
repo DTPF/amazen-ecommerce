41779e386148284f6d30185ee2e8790bd53c2188
David T. Pizarro Frick <davidpizarrofrick@gmail.com>
2023-03-13 15:01:32 +0100

    feat(serviceWorker): add functionality

--- src/index.jsx
+++ src/index.jsx
@@ -1,14 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.scss';
 import App from './App';
 // import reportWebVitals from './reportWebVitals';
+import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
+import toaster from './views/components/UI/toast/toast';
+import './index.scss';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   // <React.StrictMode>
-    <App />
+  <App />
   // </React.StrictMode>
 );
 
 // reportWebVitals(console.log)
+serviceWorkerRegistration.register({
+  onUpdate: (registration) => {
+    if (registration && registration.waiting) {
+      registration.waiting.postMessage({ type: "SKIP_WAITING" });
+      registration.update();
+      toaster('Nueva versión. Actualizando la web...');
+      window.location.reload()
+    }
+  },
+  onSuccess: () => {
+    toaster('Web disponible para su uso sin conexión')
+  },
+});
