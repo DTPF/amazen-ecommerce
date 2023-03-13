879ac050a73032944785ad20298ecbe17df1a3af
David T. Pizarro Frick <davidpizarrofrick@gmail.com>
2023-03-13 16:38:53 +0100

    remove(comments)

--- src/index.jsx
+++ src/index.jsx
@@ -1,19 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-// import reportWebVitals from './reportWebVitals';
 import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
 import toaster from './views/components/UI/toast/toast';
 import './index.scss';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  // <React.StrictMode>
   <App />
-  // </React.StrictMode>
 );
 
-// reportWebVitals(console.log)
 serviceWorkerRegistration.register({
   onUpdate: (registration) => {
     if (registration && registration.waiting) {
