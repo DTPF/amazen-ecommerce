0975a0ef7d565317423eb6d4ae9251f6f48616a1
David T. Pizarro Frick <davidpizarrofrick@gmail.com>
2023-03-13 20:28:19 +0100

    change(index.jsx): remove toaster

--- src/index.jsx
+++ src/index.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
-import toaster from './views/components/UI/toast/toast';
 import './index.scss';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -15,11 +14,6 @@ serviceWorkerRegistration.register({
     if (registration && registration.waiting) {
       registration.waiting.postMessage({ type: "SKIP_WAITING" });
       registration.update();
-      toaster('Nueva versión. Actualizando la web...');
-      window.location.reload()
     }
-  },
-  onSuccess: () => {
-    toaster('Web disponible para su uso sin conexión')
-  },
+  }
 });
