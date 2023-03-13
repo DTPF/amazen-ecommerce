c73eb426342e7fb874353db7e058ced45126238d
David T. Pizarro Frick <davidpizarrofrick@gmail.com>
2023-03-13 09:19:41 +0100

    feat(checkout): add component

---
+++ src/views/components/main/CheckoutComponent/CheckoutComponent.jsx
@@ -0,0 +1,29 @@
+import React, { useState } from 'react'
+import CartCheckout from './CartCheckout';
+import UserCheckout from './UserCheckout';
+import PaymentCheckout from './PaymentCheckout';
+import useAuthContext from '../../../../hooks/useAuthContext';
+import useCartContext from '../../../../hooks/useCartContext';
+import OrderResult from './OrderResult';
+import './CheckoutComponent.scss';
+
+export default function CheckoutComponent() {
+  const { user } = useAuthContext();
+  const { userData } = user;
+  const { cart } = useCartContext();
+  const [fetchedProducts, setFetchedProducts] = useState([]);
+  const [discount, setDiscount] = useState(0);
+
+  return (
+    <div className='checkout-component'>
+      <div className='checkout-component__row1'>
+        <CartCheckout cart={cart} fetchedProducts={fetchedProducts} setFetchedProducts={setFetchedProducts} />
+      </div>
+      <div className='checkout-component__row2'>
+        <UserCheckout userData={userData} />
+        <OrderResult cart={cart} setDiscount={setDiscount} />
+        <PaymentCheckout cart={cart} userData={userData} discount={discount} />
+      </div>
+    </div>
+  )
+}
